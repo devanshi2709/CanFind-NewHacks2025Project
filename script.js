779e7758c4d35cf24a4548c8c0bb17211c76cb50
renaqian <renaqian28@gmail.com>
2025-10-25 16:27:35 -0400

    doesnt work very well...

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+const allMarkers = [];
+let map;
+let service;
+let infowindow;
+
+
 function initMap() {
     console.log("initMap loaded!");
-    const toronto = { lat: 43.65107, lng: -79.347015 };
+    const toronto = { lat: 43.6615, lng: -79.4009 };
 
     const map = new google.maps.Map(document.getElementById("map"), {
         center: toronto,
@@ -9,17 +15,21 @@ function initMap() {
 
     const service = new google.maps.places.PlacesService(map);
     const infowindow = new google.maps.InfoWindow();
+    const allMarkers = [];
+
 
     searchRestaurants({
         location: toronto,
         radius: 5000,
         type: "restaurant",
+        keyword: "",
     }, service, map, infowindow);
 }
 
 function searchRestaurants(request, service, map, infowindow) {
     service.nearbySearch(request, (results, status, pagination) => {
         if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+
             results.forEach(place => {
                 const marker = new google.maps.Marker({
                     position: place.geometry.location,
@@ -27,6 +37,10 @@ function searchRestaurants(request, service, map, infowindow) {
                     title: place.name
                 });
                 
+                marker.price_level = place.price_level;
+                marker.types = place.types;
+                marker.name = place.name.toLowerCase();
+
                 marker.addListener("click", () => {
                     service.getDetails({
                         placeId: place.place_id,
@@ -57,9 +71,9 @@ function searchRestaurants(request, service, map, infowindow) {
                         }
                     });
                 });
+                allMarkers.push(marker);
             });
 
-            // If there are more results, fetch next page
             if (pagination && pagination.hasNextPage) {
                 setTimeout(() => {
                     pagination.nextPage();
@@ -69,4 +83,37 @@ function searchRestaurants(request, service, map, infowindow) {
             console.log("Places API status:", status);
         }
     });
+}
+
+function updateMarkers(filterFn) {
+
+    let shown = 0;
+    allMarkers.forEach(marker => {
+        if (filterFn(marker)) {
+        marker.setMap(map);
+        marker.visible = true;
+        shown++;
+        } else {
+        marker.setMap(null);
+        marker.visible = false;
+        }
+    });
+    console.log("updateMarkers -> shown:", shown, "of", allMarkers.length);
+}
+
+
+function showCheap() {
+    updateMarkers(marker => marker.price_level != null && marker.price_level <= 2 );
+}
+
+function showCanadian() {
+    updateMarkers(marker => marker.name.includes("canadian") || marker.types.includes("canadian_restaurant") );
+}
+
+function showAll() {
+    updateMarkers(marker => true );
+}
+
+function resetMarkers() {
+
 }
